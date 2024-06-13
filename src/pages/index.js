c8f22bcb9ad977f39eebe1dca5c032aaea1ec4f0
PhilLanzetta <71086048+PhilLanzetta@users.noreply.github.com>
2024-06-13 12:38:26 -0400

    aspect ratio

--- src/pages/index.js
+++ src/pages/index.js
@@ -101,6 +101,9 @@ export const query = graphql`
               videoMediaId: id
               aspectRatio
             }
+            ... on ContentfulImage {
+              id
+            }
           }
         }
       }
